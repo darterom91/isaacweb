02daa1de24cc1dba281be88dc884fa632f717557
David Artero <d.arterom@gmail.com>
2022-09-04 02:21:38 +0200

    add button menu and nav collapse

--- src/components/header_isw/Header.vue
+++ src/components/header_isw/Header.vue
@@ -1,20 +1,135 @@
 <template>
   <div class="header bg-dark container-fluid">
-    <div class="row p-2">
-      <div class="col-3 bg-primary">
-        <div class="row">
-          <div class="col ">LOGO</div>
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+      <div class="container-fluid">
+        <div class="col-6 col-sm-3 text-light display-6 fw-blod p-2 align-self-start">
+          IsaacWeB <i class="bi bi-film"></i>
+        </div>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavbar"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="col-6 col-sm-5 collapse navbar-collapse justify-content-center p-2"
+          id="collapsibleNavbar"
+        >
+          <div class="navbar-nav row border bg-light bg-opacity-75 rounded p-2">
+            <div class="nav-item col">
+              <a
+                class="nav-link text-decoration-none text-dark text-opacity-75 fw-bold"
+                href="#"
+                >Perfil</a
+              >
+            </div>
+            <div class="nav-item col">
+              <a
+                class="nav-link text-decoration-none text-dark text-opacity-75 fw-bold"
+                href="#"
+                >Galeria</a
+              >
+            </div>
+            <div class="nav-item col">
+              <a
+                class="nav-link text-decoration-none text-dark text-opacity-75 fw-bold"
+                href="#"
+                >Videos</a
+              >
+            </div>
+            <div class="nav-item col">
+              <a
+                class="nav-link text-decoration-none text-dark text-opacity-75 fw-bold"
+                href="#"
+                >Contactos</a
+              >
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-sm-3 p-2 align-self-center">
+          <div class="row justify-content-center">
+            <div class="col-2">
+              <a href="#" class="text-decoration-none text-light fw-bold"
+                ><i class="bi bi-facebook h2"></i
+              ></a>
+            </div>
+            <div class="col-2">
+              <a href="#" class="text-decoration-none text-light fw-bold"
+                ><i class="bi bi-instagram h2"></i
+              ></a>
+            </div>
+            <div class="col-2">
+              <a href="#" class="text-decoration-none text-light fw-bold"
+                ><i class="bi bi-linkedin h2"></i
+              ></a>
+            </div>
+          </div>
         </div>
       </div>
-      <div class="col-6 bg-danger">
+    </nav>
+
+    <!-- <div class="row py-2 px-1 text-center justify-content-around">
+      <div
+        class="col-3 col-sm-3 text-light display-6 fw-blod p-2 align-self-start"
+      >
+        IsaacWeB <i class="bi bi-film"></i>
+      </div>
+      <div
+        class="col-8 col-sm-4 border bg-light bg-opacity-75 rounded align-self-start align-self-center p-2"
+      >
         <div class="row">
-          <div class="col border"><a href="#" class="text-decoration-none text-dark">Perfil</a></div>
-          <div class="col border">Galeria</div>
-          <div class="col border">Videos</div>
-          <div class="col border">Contactos</div>
+          <div class="col">
+            <a
+              href="#"
+              class="text-decoration-none text-dark text-opacity-75 fw-bold"
+              >Perfil</a
+            >
+          </div>
+          <div class="col">
+            <a
+              href="#"
+              class="text-decoration-none text-dark text-opacity-75 fw-bold"
+              >Galeria</a
+            >
+          </div>
+          <div class="col">
+            <a
+              href="#"
+              class="text-decoration-none text-dark text-opacity-75 fw-bold"
+              >Videos</a
+            >
+          </div>
+          <div class="col">
+            <a
+              href="#"
+              class="text-decoration-none text-dark text-opacity-75 fw-bold"
+              >Contactos</a
+            >
+          </div>
         </div>
       </div>
-    </div>
+      <div class="col-12 col-sm-3 p-2 align-self-center">
+        <div class="row justify-content-center">
+          <div class="col-2">
+            <a href="#" class="text-decoration-none text-light fw-bold"
+              ><i class="bi bi-facebook h2"></i
+            ></a>
+          </div>
+          <div class="col-2">
+            <a href="#" class="text-decoration-none text-light fw-bold"
+              ><i class="bi bi-instagram h2"></i
+            ></a>
+          </div>
+          <div class="col-2">
+            <a href="#" class="text-decoration-none text-light fw-bold"
+              ><i class="bi bi-linkedin h2"></i
+            ></a>
+          </div>
+        </div>
+      </div>
+    </div> -->
   </div>
 </template>
 
@@ -22,6 +137,4 @@
 export default {};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
