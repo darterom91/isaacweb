d8c57cdf808c95136f958baf57b118c9c16f2e4a
David Artero <d.arterom@gmail.com>
2022-09-13 20:12:01 +0200

    add and modify footer

--- src/components/footer_isw/Footer.vue
+++ src/components/footer_isw/Footer.vue
@@ -1,17 +1,37 @@
 <template>
-  <div class="Footer">
-    <div class="row">
-      <div class="col">Footer IsaacWeb</div>
+  <div class="Footer container-fluid">
+    <div class="row justify-content-around">
+      <div class="col-12 ol-sm-12 col-md-6 align-self-center p-2">
+        <a href="#" class="text-decoration-none display-6 text-light fw-blod">
+          IsaacWeB <i class="bi bi-film"></i
+        ></a>
+      </div>
+      <div class="col-12 col-sm-12 col-md-6 align-self-center p-2">
+        <div class="row justify-content-center">
+          <div class="col-2">
+            <a href="#" class="text-decoration-none text-light fw-bold"
+              ><i class="bi bi-facebook h2"></i
+            ></a>
+          </div>
+          <div class="col-2">
+            <a href="#" class="text-decoration-none text-light fw-bold"
+              ><i class="bi bi-instagram h2"></i
+            ></a>
+          </div>
+          <div class="col-2">
+            <a href="#" class="text-decoration-none text-light fw-bold"
+              ><i class="bi bi-linkedin h2"></i
+            ></a>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 border-top border-light align-self-center p-2">COPYRIGHT Darterom91 2022</div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
